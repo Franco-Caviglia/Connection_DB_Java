20f96bee5721c778258c6373718d6243917a114b
Franco-Caviglia <francocaviglia77@gmail.com>
2023-06-09 10:04:50 -0300

    cliente package

--- App/src/App/App.java
+++ App/src/App/App.java
@@ -1,5 +1,9 @@
+package App;
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
---
+++ App/src/Cliente/Cliente.java
@@ -0,0 +1,46 @@
+package Cliente;
+
+public class Cliente {
+    
+    //Generamos variables que utilizaremos tambien en nuestra BD;
+    private String name;
+    private String lastName;
+    private String phone;
+    private String email;
+
+    //Generamos getters & setters para almacenar y obtener los datos;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    
+}
